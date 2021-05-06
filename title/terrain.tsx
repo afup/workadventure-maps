0389487d64ad24f53b0de75e502d442578e2f880
Mathieu Lemoine <kura@kura.ooo>
2021-05-07 01:50:07 +0200

    🍱 added terrain tiles

---
+++ title/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="terrain" tilewidth="32" tileheight="32" tilecount="54" columns="6">
+ <image source="terrain.png" width="192" height="288"/>
+</tileset>
